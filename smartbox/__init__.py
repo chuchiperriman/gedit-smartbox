b89d08d162fac4bc1ecc82a285836333207fdd4f
Jesús Barbero Rodríguez <chuchiperriman@gmail.com>
2013-06-09 02:37:32 +0200

    Added tools menu to open the smart box

--- smartbox/__init__.py
+++ smartbox/__init__.py
@@ -9,21 +9,46 @@ class SmartBoxPlugin(GObject.Object, Gedit.WindowActivatable):
     
     def do_activate(self):
         self.manager = SmartBoxManager.get_instance()
-        self.manager.add_provider(ExampleProvider())
-        self.timeout_id = GObject.timeout_add(1000, self.on_timeout, None)
+        self.timeout_id = GObject.timeout_add(5000, self.on_timeout, None)
+        
+        self.insert_menu()
 
     def do_deactivate(self):
-        pass
+        self.remove_menu()
 
     def do_update_state(self):
         pass
         
-    def on_timeout(self, user_data):
+    def on_timeout(self, user_data=None):
         print '*' * 50
         for p in self.manager.providers:
             print p.get_name()
         return True
+        
+    def insert_menu(self):
+        manager = self.window.get_ui_manager()
 
+        self.action_group = Gtk.ActionGroup("GeditSmartBoxPluginActions")
+        self.action_group.set_translation_domain('gedit')
+        self.action_group.add_actions([('OpenBox', None,
+                        'Open Smart _Box...', \
+                        None, 'Open Smart Box', \
+                        self.on_action_open_activate)])
+
+        self.merge_id = manager.new_merge_id()
+        manager.insert_action_group(self.action_group, -1)
+        manager.add_ui(self.merge_id, '/MenuBar/ToolsMenu/ToolsOps_5', \
+                        'OpenBox', 'OpenBox', Gtk.UIManagerItemType.MENUITEM, False)
+                        
+    def remove_menu(self):
+        manager = self.window.get_ui_manager()
+        manager.remove_ui(self.merge_id)
+        manager.remove_action_group(self.action_group)
+        self.action_group = None
+                
+    def on_action_open_activate(self, action):
+        self.on_timeout()
+        
 class Singleton:
     """
     A non-thread-safe helper class to ease implementing singletons.
@@ -67,14 +92,7 @@ class Singleton:
 class SmartBoxManager(object):
     
     def __init__(self):
-        print 'iniiiiiiiiiiiit'
         self.providers = []
         
     def add_provider(self, provider):
-        self.providers.append(provider)
-       
-class ExampleProvider (object):
-
-    def get_name(self):
-        return "Example"
-        
+        self.providers.append(provider)       
